5bfa44053e489da79c1a0b1497b230490c299f85
izen1231 <izen1231@gmail.com>
2020-07-08 01:49:32 +0900

    stt bixby error

--- app/src/main/java/com/pongponglabs/eyear/MainActivity.kt
+++ app/src/main/java/com/pongponglabs/eyear/MainActivity.kt
@@ -2,11 +2,15 @@ package com.pongponglabs.eyear
 
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
@@ -14,42 +18,75 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    private var txtResult: TextView? = null
+    private val TAG = "TAG"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txtResult = findViewById(R.id.txtResult)
 
         checkPermission()
         startSpeechToText()
     }
 
-    @SuppressLint("ClickableViewAccessibility")
     private fun startSpeechToText() {
-
         val speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
         val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
         speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, 10)
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show()
+        }
 
         speechRecognizer.setRecognitionListener(object : RecognitionListener {
-            override fun onReadyForSpeech(bundle: Bundle) {}
+            override fun onReadyForSpeech(params: Bundle) {
+                Log.d(TAG, "onReadyForSpeech")
+            }
 
-            override fun onBeginningOfSpeech() {}
+            override fun onBeginningOfSpeech() {
+                Log.d(TAG, "onBeginningOfSpeech")
+            }
 
-            override fun onRmsChanged(v: Float) {}
+            override fun onRmsChanged(rmsdB: Float) {
+                Log.d(TAG, "onRmsChanged")
+            }
 
-            override fun onBufferReceived(bytes: ByteArray) {}
+            override fun onBufferReceived(buffer: ByteArray) {
+                Log.d(TAG, "onBufferReceived")
+            }
 
-            override fun onEndOfSpeech() {}
+            override fun onEndOfSpeech() {
+                Log.d(TAG, "onEndofSpeech")
+            }
 
-            override fun onError(i: Int) {}
+            override fun onError(error: Int) {
+                Log.e(TAG, "error $error")
+            }
 
             override fun onResults(bundle: Bundle) {}
 
-            override fun onPartialResults(bundle: Bundle) {}
+            override fun onPartialResults(bundle: Bundle) {
+                Log.d(TAG, "onPartialResults")
+            }
 
-            override fun onEvent(i: Int, bundle: Bundle) {}
+            override fun onEvent(i: Int, bundle: Bundle) {
+                Log.d(TAG, "onEvent $i")
+            }
         })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            10 -> if (resultCode == Activity.RESULT_OK && data != null) {
+                val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                txtResult!!.text = result!![0]
+                Log.d("TAG", result[0])
+            }
+        }
     }
 
     var permissionlistener: PermissionListener = object : PermissionListener {
@@ -71,3 +108,5 @@ class MainActivity : AppCompatActivity() {
             .check()
     }
 }
+
+
