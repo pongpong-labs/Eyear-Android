7de9a0950c0f7c4792268c181ddeb293af87f3a9
izen1231 <izen1231@gmail.com>
2020-07-08 00:30:50 +0900

    stt add

--- app/src/main/java/com/pongponglabs/eyear/MainActivity.kt
+++ app/src/main/java/com/pongponglabs/eyear/MainActivity.kt
@@ -1,11 +1,73 @@
 package com.pongponglabs.eyear
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import androidx.appcompat.app.AppCompatActivity
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkPermission()
+        startSpeechToText()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun startSpeechToText() {
+
+        val speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+
+        speechRecognizer.setRecognitionListener(object : RecognitionListener {
+            override fun onReadyForSpeech(bundle: Bundle) {}
+
+            override fun onBeginningOfSpeech() {}
+
+            override fun onRmsChanged(v: Float) {}
+
+            override fun onBufferReceived(bytes: ByteArray) {}
+
+            override fun onEndOfSpeech() {}
+
+            override fun onError(i: Int) {}
+
+            override fun onResults(bundle: Bundle) {}
+
+            override fun onPartialResults(bundle: Bundle) {}
+
+            override fun onEvent(i: Int, bundle: Bundle) {}
+        })
+    }
+
+    var permissionlistener: PermissionListener = object : PermissionListener {
+        override fun onPermissionGranted() {
+
+        }
+
+        override fun onPermissionDenied(deniedPermissions: MutableList<String>) {
+
+        }
+    }
+
+    fun checkPermission(){
+        TedPermission.with(this)
+            .setPermissionListener(permissionlistener)
+            .setRationaleMessage("앱의 기능을 사용하기 위해서는 권한이 필요합니다.")
+            .setDeniedMessage("[설정] > [권한] 에서 권한을 허용할 수 있습니다.")
+            .setPermissions(android.Manifest.permission.INTERNET,android.Manifest.permission.RECORD_AUDIO)
+            .check()
     }
 }
