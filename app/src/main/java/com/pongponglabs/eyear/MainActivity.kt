ad12579ca670b0dfd65b53a5cdd9eae296a539d6
izen1231 <izen1231@gmail.com>
2020-07-18 14:46:41 +0900

    gradle update & stt fix

--- app/src/main/java/com/pongponglabs/eyear/MainActivity.kt
+++ app/src/main/java/com/pongponglabs/eyear/MainActivity.kt
@@ -1,93 +1,67 @@
 package com.pongponglabs.eyear
 
 
-import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
-import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
-import android.util.Log
-import android.widget.TextView
+import android.speech.tts.TextToSpeech
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    private var txtResult: TextView? = null
-    private val TAG = "TAG"
+    companion object {
+        private const val REQUEST_CODE_STT = 1
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        txtResult = findViewById(R.id.txtResult)
 
         checkPermission()
-        startSpeechToText()
-    }
 
-    private fun startSpeechToText() {
-        val speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
-        val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivityForResult(intent, 10)
-        } else {
-            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show()
+        btn_stt.setOnClickListener {
+            val sttIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            sttIntent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            sttIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "말하세요!")
+
+
+            try {
+                startActivityForResult(sttIntent, REQUEST_CODE_STT)
+            } catch (e: ActivityNotFoundException) {
+                e.printStackTrace()
+                Toast.makeText(this, "Your device does not support STT.", Toast.LENGTH_LONG).show()
+            }
         }
-
-        speechRecognizer.setRecognitionListener(object : RecognitionListener {
-            override fun onReadyForSpeech(params: Bundle) {
-                Log.d(TAG, "onReadyForSpeech")
-            }
-
-            override fun onBeginningOfSpeech() {
-                Log.d(TAG, "onBeginningOfSpeech")
-            }
-
-            override fun onRmsChanged(rmsdB: Float) {
-                Log.d(TAG, "onRmsChanged")
-            }
-
-            override fun onBufferReceived(buffer: ByteArray) {
-                Log.d(TAG, "onBufferReceived")
-            }
-
-            override fun onEndOfSpeech() {
-                Log.d(TAG, "onEndofSpeech")
-            }
-
-            override fun onError(error: Int) {
-                Log.e(TAG, "error $error")
-            }
-
-            override fun onResults(bundle: Bundle) {}
-
-            override fun onPartialResults(bundle: Bundle) {
-                Log.d(TAG, "onPartialResults")
-            }
-
-            override fun onEvent(i: Int, bundle: Bundle) {
-                Log.d(TAG, "onEvent $i")
-            }
-        })
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
-            10 -> if (resultCode == Activity.RESULT_OK && data != null) {
-                val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-                txtResult!!.text = result!![0]
-                Log.d("TAG", result[0])
+            REQUEST_CODE_STT -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    result?.let {
+                        val recognizedText = it[0]
+                        output.setText(recognizedText)
+                    }
+                }
             }
         }
     }
+
 
     var permissionlistener: PermissionListener = object : PermissionListener {
         override fun onPermissionGranted() {
