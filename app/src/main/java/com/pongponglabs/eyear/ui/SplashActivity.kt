037ab66f4141f8e68d9abc38463ac8896b5056ef
izen1231 <izen1231@gmail.com>
2020-07-27 00:33:27 +0900

    splash image add, spoqa font add

--- app/src/main/java/com/pongponglabs/eyear/ui/SplashActivity.kt
+++ app/src/main/java/com/pongponglabs/eyear/ui/SplashActivity.kt
@@ -1,12 +1,24 @@
 package com.pongponglabs.eyear.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.pongponglabs.eyear.R
 
 class SplashActivity : AppCompatActivity() {
+
+    val SPLASH_TIME: Long = 1500
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+
+        }, SPLASH_TIME)
     }
 }
