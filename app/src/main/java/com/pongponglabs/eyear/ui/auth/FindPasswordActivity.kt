0cb532e538da60ee6cb786a4145fa237d6969825
izen1231 <izen1231@gmail.com>
2020-07-29 18:24:03 +0900

    password reset page add

--- app/src/main/java/com/pongponglabs/eyear/ui/auth/FindPasswordActivity.kt
+++ app/src/main/java/com/pongponglabs/eyear/ui/auth/FindPasswordActivity.kt
@@ -1,7 +1,6 @@
 package com.pongponglabs.eyear.ui.auth
 
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.pongponglabs.eyear.R
 
@@ -10,11 +9,5 @@ class FindPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_password)
-
-
-        val actionBar: ActionBar? = supportActionBar //제목줄 객체 얻어오기
-        actionBar?.setDisplayHomeAsUpEnabled(true) //업버튼 <- 만들기
-
-
     }
 }
--- app/src/main/java/com/pongponglabs/eyear/ui/auth/LoginPageActivity.kt
+++ app/src/main/java/com/pongponglabs/eyear/ui/auth/LoginPageActivity.kt
@@ -1,12 +1,20 @@
 package com.pongponglabs.eyear.ui.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pongponglabs.eyear.R
+import kotlinx.android.synthetic.main.activity_login_page.*
 
 class LoginPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+        forgotPassword.setOnClickListener{
+            val intent = Intent(this, FindPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
