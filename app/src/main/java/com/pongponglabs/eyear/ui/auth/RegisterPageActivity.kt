08ff14be7945ca97a1a8fddc380e808fc4968a64
izen1231 <izen1231@gmail.com>
2020-07-29 16:51:05 +0900

    login page layout add

---
+++ app/src/main/java/com/pongponglabs/eyear/ui/auth/RegisterPageActivity.kt
@@ -0,0 +1,12 @@
+package com.pongponglabs.eyear.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pongponglabs.eyear.R
+
+class RegisterPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_page)
+    }
+}
