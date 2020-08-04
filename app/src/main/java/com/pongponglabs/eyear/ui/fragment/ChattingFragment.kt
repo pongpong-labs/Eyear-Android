f123c2c499dcbec5faa9b51b331ee2ea8b0b9333
izen1231 <izen1231@gmail.com>
2020-08-05 01:46:09 +0900

    TODO fragment error fix

--- app/src/main/java/com/pongponglabs/eyear/ui/fragment/ChattingFragment.kt
+++ app/src/main/java/com/pongponglabs/eyear/ui/fragment/ChattingFragment.kt
@@ -5,15 +5,67 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.pongponglabs.eyear.R
+import com.pongponglabs.eyear.ui.fragment.message.AlertFragment
+import com.pongponglabs.eyear.ui.fragment.message.MessageFragment
+import kotlinx.android.synthetic.main.activity_chatting_fragment.*
 
 class ChattingFragment : Fragment() {
 
     lateinit var mView: View
+    private var mViewPager: ViewPager? = null
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
         mView = inflater.inflate(R.layout.activity_chatting_fragment, container, false)
 
+        val fragmentManager = (activity)!!.supportFragmentManager
+        mViewPager = view!!.findViewById(R.id.viewpager)
+        mViewPager!!.adapter =
+            PagerAdapter(
+                fragmentManager
+            )
+        mViewPager!!.currentItem = 1
+
+        val tabLayout = view!!.findViewById<View>(R.id.tabLayout) as TabLayout
+        tabLayout.setupWithViewPager(mViewPager)
+
+        tabLayout.getTabAt(0)!!.setText("메세지")
+        tabLayout.getTabAt(1)!!.setText("알림")
+        mViewPager!!.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) = tab.select()
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
+
         return mView
+    }
+    class PagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+        override fun getItem(position: Int): Fragment {
+
+            return when (position) {
+                0 ->
+                    MessageFragment()
+                1 ->
+                    AlertFragment()
+                else ->
+                    AlertFragment()
+            }
+        }
+
+        override fun getCount(): Int = 2
     }
 }
---
+++ app/src/main/java/com/pongponglabs/eyear/ui/fragment/message/MessageFragment.kt
@@ -0,0 +1,16 @@
+package com.pongponglabs.eyear.ui.fragment.message
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pongponglabs.eyear.R
+
+class MessageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_message, container, false)
+    }
+}
