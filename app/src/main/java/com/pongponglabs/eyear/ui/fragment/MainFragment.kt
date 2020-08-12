763c64ef942631c0ddfd5e6bf58ab151906e09c1
izen1231 <izen1231@gmail.com>
2020-08-12 13:26:12 +0900

    script page add

--- app/src/main/java/com/pongponglabs/eyear/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/pongponglabs/eyear/ui/fragment/MainFragment.kt
@@ -1,11 +1,14 @@
 package com.pongponglabs.eyear.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.pongponglabs.eyear.R
+import com.pongponglabs.eyear.ui.script.ScriptActivity
+import kotlinx.android.synthetic.main.activity_main_fragment.view.*
 
 class MainFragment : Fragment() {
 
@@ -13,6 +16,11 @@ class MainFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         mView = inflater.inflate(R.layout.activity_main_fragment, container, false)
+
+        mView.fab.setOnClickListener{
+            val intent = Intent(this@MainFragment.context, ScriptActivity::class.java)
+            startActivity(intent)
+        }
 
         return mView
     }
---
+++ app/src/main/java/com/pongponglabs/eyear/ui/script/ScriptActivity.kt
@@ -0,0 +1,12 @@
+package com.pongponglabs.eyear.ui.script
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pongponglabs.eyear.R
+
+class ScriptActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_script)
+    }
+}
