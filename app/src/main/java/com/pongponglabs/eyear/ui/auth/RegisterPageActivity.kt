9a4d4b6910805bc2a13cc6743d9bd645b8aec57c
izen1231 <izen1231@gmail.com>
2020-08-20 01:46:36 +0900

    null check

--- app/src/main/java/com/pongponglabs/eyear/ui/auth/RegisterPageActivity.kt
+++ app/src/main/java/com/pongponglabs/eyear/ui/auth/RegisterPageActivity.kt
@@ -1,7 +1,9 @@
 package com.pongponglabs.eyear.ui.auth
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -12,7 +14,9 @@ import com.pongponglabs.eyear.R
 import com.pongponglabs.eyear.api.RetrofitClient
 import com.pongponglabs.eyear.api.data.Users
 import com.pongponglabs.eyear.ui.MainActivity
+import kotlinx.android.synthetic.main.activity_login_page.*
 import kotlinx.android.synthetic.main.activity_register_page.*
+import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -33,8 +37,6 @@ class RegisterPageActivity : AppCompatActivity() {
             join()
 
         }
-
-
 
 
         val university = arrayOf("부산대학교", "부산가톨릭대학교", "부산교육대학교", "부산여자대학교", "부산외국어대학교")
@@ -69,6 +71,12 @@ class RegisterPageActivity : AppCompatActivity() {
     }
 
     private fun join() {
+        val name = inputRegisterName.text.toString()
+        val uid = inputRegisterId.text.toString()
+        val email = inputRegisterEmail.text.toString()
+        val password = inputRegisterPassword.text.toString()
+        val passwordValidator = inputRegisterPasswordValidator.text.toString()
+
         val join = HashMap<String, Any>()
         join["name"] = inputRegisterName.text.toString().trim()
         join["uid"] = inputRegisterId.text.toString().trim()
@@ -79,23 +87,54 @@ class RegisterPageActivity : AppCompatActivity() {
         join["dept"] = 1
 
 
+        if (TextUtils.isEmpty(name)) {
+            inputRegisterName.error = "이름을 입력하세요."
+        }
+        if (TextUtils.isEmpty(uid)) {
+            inputRegisterId.error = "아이디를 입력하세요."
+        }
+        if (TextUtils.isEmpty(email)) {
+            inputRegisterEmail.error = "이메일를 입력하세요."
+        }
+        if (TextUtils.isEmpty(password)) {
+            inputRegisterPassword.error = "비밀번호를 입력하세요."
+        }
+        if (TextUtils.isEmpty(passwordValidator)){
+            inputRegisterPasswordValidator.error = "비밀번호를 입력하세요"
+        }
 
-        RetrofitClient.retrofitService.join(join).enqueue(object : Callback<Users> {
-            override fun onResponse(call: Call<Users>?, response: Response<Users>?) {
-                when (response!!.code()) {
-                    200 -> {
-                        Toast.makeText(this@RegisterPageActivity, "회원가입에 성공했습니다.", Toast.LENGTH_SHORT).show()
-                        startActivity(Intent(this@RegisterPageActivity, MainActivity::class.java))
-                        finish()
-                    }
-                    400 -> {
-                        Toast.makeText(this@RegisterPageActivity, "오류가 발생하였습니다.", Toast.LENGTH_SHORT).show()
+        if (!TextUtils.isEmpty(uid) && !TextUtils.isEmpty(password)  && !TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(passwordValidator)) {
+            RetrofitClient.retrofitService.join(join).enqueue(object : Callback<Users> {
+                override fun onResponse(call: Call<Users>?, response: Response<Users>?) {
+                    when (response!!.code()) {
+                        200 -> {
+                            Toast.makeText(
+                                this@RegisterPageActivity,
+                                "회원가입에 성공했습니다. ${response.body()!!.name} 님",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            startActivity(
+                                Intent(
+                                    this@RegisterPageActivity,
+                                    MainActivity::class.java
+                                )
+                            )
+                            finish()
+                        }
+                        400 -> {
+                            Toast.makeText(
+                                this@RegisterPageActivity,
+                                "오류가 발생하였습니다.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     }
                 }
-            }
-            override fun onFailure(call: Call<Users>?, t: Throwable?) {
 
-            }
-        })
+                override fun onFailure(call: Call<Users>?, t: Throwable?) {
+
+                }
+            })
+        }
     }
 }
