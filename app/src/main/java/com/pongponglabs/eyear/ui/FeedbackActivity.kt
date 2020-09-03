8b511af507548d6c1f200fa5e46dc0de98817af3
izen1231 <izen1231@gmail.com>
2020-09-03 16:00:38 +0900

    script dialog add, layout fix

--- app/src/main/java/com/pongponglabs/eyear/ui/FeedbackActivity.kt
+++ app/src/main/java/com/pongponglabs/eyear/ui/FeedbackActivity.kt
@@ -1,12 +1,23 @@
 package com.pongponglabs.eyear.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.pongponglabs.eyear.R
+import com.pongponglabs.eyear.ui.fragment.MainFragment
+import kotlinx.android.synthetic.main.activity_save_script_dialog.*
 
 class FeedbackActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feedback)
+
+        saveScriptBtn.setOnClickListener{
+            val intent = Intent(this, MainFragment::class.java)
+            Toast.makeText(this@FeedbackActivity, "스크립트가 저정되었습니다.", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+            finish()
+        }
     }
 }
